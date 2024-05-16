815d1cdba9224748276de32cf887c4453a963410
diepdp <diepdp@kaiyouit.com>
2024-05-16 13:47:51 +0700

    update cross

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/calculate_tokens": {"origins": "http://localhost:8080"}}) 
 
 @app.route('/calculate_tokens', methods=['POST'])
 def calculate_tokens():
@@ -23,9 +25,7 @@ def calculate_tokens():
     # Đây chỉ là ví dụ giả định, bạn cần thay thế bằng logic thật của bạn
     token_count = len(prompt.split())
 
-    response = make_response(jsonify({"tokens": token_count}))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return jsonify({"tokens": token_count})
 
 if __name__ == '__main__':
     app.run(debug=True)
